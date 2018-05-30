2c05e74522c80bd232e5960802c86f975212275d
TABIJO Risa <33144563+risa27primadoll@users.noreply.github.com>
2018-05-30 21:23:41 +0800

    Add files via upload
    途中経過です。　デザインはまだ完成してないです。

--- signin.php
+++ signin.php
@@ -6,15 +6,14 @@
   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
 </head>
-<body style="background-color:ivory" style="margin-top: 60px">
+<body  style="background-color:ivory" style="margin-top: 60px">
   <div class="container">
     <div class="row">
       <div class="col-xs-8 col-xs-offset-2 thumbnail">
         <h2 class="text-center content_header">日々<br><br>
-        サインアップ</h2>
-        <form method="POST" action="check.php" enctype="multipart/form-data">
+        サインイン</h2>
+        <form method="POST" action="private.php" enctype="multipart/form-data">
           <div class="form-group">
             <label for="nickname">ニックネーム</label><br>
             <input type="nickname" name="input_nickname" class="form-control" id="nickname" placeholder="">
@@ -26,16 +25,11 @@
             <p class="text-danger">パスワードを入力してください</p>
             <?php } ?>
             <?php if(isset($errors['password'])&& $errors['password']=='length'){ ?>
-            <p class="text-danger">パスワードは4~16文字で入力してください</p>
+            <p class="text-danger">パスワードは4~16文字で入力してくださいね</p>
             <?php } ?>
           </div>
           <div class="form-group">
-            <label for="password">卒業日</label><br>
-            <input type="date" name="example1">
-            <p class="text-danger">卒業日を入力してください</p>
-          </div>
-          <div class="form-group">
-            <label for="img_name">プロフィール画像</label>
+            <label for="img_name">日々を投稿する</label>
             <input type="file" name="input_img_name" id="img_name" accept="image/*">
             <?php if(isset($errors['img_name'])&& $errors['img_name']=='blank'){ ?>
             <p class="text-danger">画像を選択してください</p>
@@ -44,7 +38,7 @@
             <p class="text-danger">拡張子が「jpg」「jpeg」「png」「gif」の画像を選択してください</p>
             <?php } ?>
           </div>
-          <input type="submit" class="btn2" value="サインアップ">
+          <input type="submit" class="btn btn-info" value="サインイン">
         </form>
       </div>
     </div>
