06808c4bae169042c41181d9a6cf86c9ceb45a90
risa caldino mashimo <diva.27.primadoll@gmail.com>
2018-06-06 10:40:47 +0800

    変更中

--- alumnus.php
+++ alumnus.php
@@ -7,40 +7,183 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./assets/css/sample.css">
-    
-    <title>日々</title>
-  </head>
 
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> 
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/alumnus.css">
+    
+
+
+    <title>Hello, world!</title>
+  </head>
   <body>
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-2 sidebar1">
           <div class="logo">
             <img src="assets/img/hibilog.png" class="hibilogo" alt="Logo">
-          </div><!--logo-->
+          </div>
             <br>
           <div class="left-navigation">
-            <ul class="list">
-                <li>はじめに</li>
-                <li>ネクシード生の日々</li>
-                <li>卒業生の日々</li>
-                <li>今週のお題</li>
-                <li>マイページ</li>
+             <ul class="list">
+              <li><a href="" >はじめに</a></li>
+              <li><a href="cur_student.php" >ネクシード生の日々</a></li>
+              <li><a href="alumnus" >卒業生の日々</a></li>
+              <li><a href="view.php" >今週のお題</a></li>
+              <li><a href="my_page.php" >マイページ</a></li>
             </ul>
-          </div><!--left-navigation-->
-        </div><!--sidebar1-->
-        <div class="col-md-2"></div>
-        <!--ここから本文をかけるよ-->
-        <div class="col-md-10 main-content">
-          ここに入力する
+          </div>
         </div>
-      </div><!--row-->
-    </div><!--container-->
+        <div class="col-md-2"></div>
+        <div class="col-md-10 main-content">
+          <div class="row">
+            <div class="col-md-12 ">
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+            </div>
+          </div>
+          <div class="row concept">
+            <div class="col-md-12">
+              <h1 class="hibi now">過去の日々</h1>
+              <p>ネクシード卒業生の日々の一覧ページ</p>
+              <!-- <p class="hibi subconcept">大切な「日々」の思い出をカタチにできるサービスです。</p> -->
+            </div>
+          </div>
+          <div class="row">
+            <!-- <div class="col-md-offset-1 col-md-10"> -->
+              <div class="col-md-10 ">
+              <div class="row">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-10 today_hibi">
+              </h5>
+              <div class="row">
+              <div class="col-md-3" >
+                  <img src="assets/img/LRG_DSC05296.jpg"  style="border: solid 3px;margin: 10px;width: 110%;">
+                  <p>toshiki123</p>
+                  <p>俺のHTML最高っしょ</p>
+                </div>
+                <div class="col-md-3">
+                  <li class="post-load-index">
+              <div class="contents-list-inner">
+                
+                <div class="text contents-list-text">
+                  <p class="text-ellipsis multiline">
+                  </p>
+                </div>
+              </div>
+            </li>
+                  <img src="assets/img/LRG_DSC05227.jpg" class="contents-hibi_pic">
+                  <p>kodai333</p>
+                  <p>朝のパンって美味しいな〜</p>
+                </div>
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05046.jpg" class="hibi_pic">
+                  <p>emily888</p>
+                  <p>今日のナイトマーケット</p>
+                </div>
+                <!-- <div class="row"> -->
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>amibanana1</p>
+                  <p>俺のHTML最高っしょ</p>
+                </div>
+              </div>  <!-- rowの -->
+          <div class="row">
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>toshiki123</p>
+                  <p>俺のHTML最高っしょ</p>
+                </div>
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05227.jpg" class="hibi_pic">
+                  <p>kodai333</p>
+                  <p>朝のパンって美味しいな〜</p>
+                </div>
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05046.jpg" class="hibi_pic">
+                  <p>emily888</p>
+                  <p>今日のナイトマーケット</p>
+                </div>
+                <!-- <div class="row"> -->
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>amibanana1</p>
+                  <p>俺のHTML最高っしょ</p>
+                </div>
+              </div>
+            </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>toshiki123</p>
+                  <p>俺のHTML最高っしょ</p>
+                </div>
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05227.jpg" class="hibi_pic">
+                  <p>kodai333</p>
+                  <p>朝のパンって美味しいな〜</p>
+                </div>
+               <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05046.jpg" class="hibi_pic">
+                  <p>emily888</p>
+                  <p>今日のナイトマーケット</p>
+                </div>
+                <!-- <div class="row"> -->
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>amibanana1</p>
+                  <p>俺のHTML最高っしょ</p>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>toshiki123</p>
+                  <p>俺のHTML最高っしょ</p>
+                 </div>
+                 <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05227.jpg" class="hibi_pic">
+                  <p>kodai333</p>
+                  <p>朝のパンって美味しいな〜</p>
+                 </div>
+                <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05046.jpg" class="hibi_pic">
+                  <p>emily888</p>
+                  <p>今日のナイトマーケット</p>
+                 </div>
+                <!-- <div class="row"> -->
+                 <div class="col-md-3" id="imgbox">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>amibanana1</p>
+                  <p>俺のHTML最高っしょ</p>
+                 </div>
+                </div>
+                <div aria-label="Page navigation">
+               <ul class="pager">
+
+      
+            <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+          
+
+            <li class="next disabled"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+          
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+          
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
   </body>
 </html>
--- cur_student.php
+++ cur_student.php
@@ -11,7 +11,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> 
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/hibi.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/alumnus.css">
     
 
 
@@ -26,53 +26,34 @@
           </div>
             <br>
           <div class="left-navigation">
-            <ul class="list">
-                <li>はじめに</li>
-                <li>ネクシード生の日々</li>
-                <li>卒業生の日々</li>
-                <li>今週のお題</li>
-                <li>マイページ</li>
+             <ul class="list">
+              <li><a href="" >はじめに</a></li>
+              <li><a href="cur_student.php" >ネクシード生の日々</a></li>
+              <li><a href="alumnus" >卒業生の日々</a></li>
+              <li><a href="view.php" >今週のお題</a></li>
+              <li><a href="my_page.php" >マイページ</a></li>
             </ul>
           </div>
         </div>
         <div class="col-md-2"></div>
         <div class="col-md-10 main-content">
           <div class="row">
-            <div class="col-md-12 top">
+            <div class="col-md-12 ">
 
             </div>
           </div>
           <div class="row concept">
             <div class="col-md-12">
-              <h1 class="hibi mainconcept">今の日々</h1>
-              <p>ネクシード生の日々の一覧ページ</p>
+              <h1 class="hibi now">の日々</h1>
+              <p>ネクシード卒業生の日々の一覧ページ</p>
               <!-- <p class="hibi subconcept">大切な「日々」の思い出をカタチにできるサービスです。</p> -->
             </div>
           </div>
           <div class="row">
             <!-- <div class="col-md-offset-1 col-md-10"> -->
               <div class="col-md-1"></div>
-              <div class="col-md-10 explain">
+              <div class="col-md-10 ">
               <div class="row">
-                
-                <!-- <div class="col-md-6 hibi explain_text">
-                <p>言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉Z</p>
-                </div> -->
-                
-<!--                 <div class="col-md-6"> -->
-                <!-- <p>写真が入る</p> -->
-               <!--  <img src="img/hibilogopng.png" class="hibi explain_pic">
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6"> -->
-                  <!-- <p>写真が入る</p> -->
-                 <!--  <img src="img/hibi.jpg" class="hibi explain_pic"> -->
-
-                <!-- </div>
-                <div class="col-md-6">
-                  <p class="hibi explain_text">言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉が入る。言葉</p>
-                </div> -->
               </div>
             </div>
           </div>
@@ -81,14 +62,26 @@
             <div class="col-md-10 today_hibi">
               </h5>
               <div class="row">
-                <div class="col-md-4">
-                  <img src="img/kim.jpg" class="hibi_pic">
+                <div class="col-xs-3">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>toshiki123</p>
+                  <p>俺のHTML最高っしょ</p>
                 </div>
-                <div class="col-md-4">
-                  <img src="img/kim.jpg" class="hibi_pic">
+                <div class="col-xs-3">
+                  <img src="assets/img/LRG_DSC05227.jpg" class="hibi_pic">
+                  <p>kodai333</p>
+                  <p>朝のパンって美味しいな〜</p>
                 </div>
-                <div class="col-md-4">
-                  <img src="img/kim.jpg" class="hibi_pic">
+                <div class="col-xs-3">
+                  <img src="assets/img/LRG_DSC05046.jpg" class="hibi_pic">
+                  <p>emily888</p>
+                  <p>今日のナイトマーケット</p>
+                </div>
+                <div class="row">
+                <div class="col-xs-3">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>amibanana1</p>
+                  <p>俺のHTML最高っしょ</p>
                 </div>
               </div>
             </div>
@@ -98,20 +91,89 @@
             <div class="col-md-10 topic_hibi">
               <div class="row">
                 <div class="col-md-4">
-                  <img src="img/kim.jpg" class="hibi_pic">
+                  <img src="assets/img/LRG_DSC04965.jpg" class="hibi_pic">
+                  <P>yosimerge6</P>
+                  <p>今日の夕食はローカルフード</p>
                 </div>
                 <div class="col-md-4">
-                  <img src="img/kim.jpg" class="hibi_pic">
+                  <img src="assets/img/IMG_4622.jpg" class="hibi_pic">
+                  <p>risa27</p>
+                  <p>シュリンプ美味すぎ</p>
                 </div>
                 <div class="col-md-4">
-                  <img src="img/kim.jpg" class="hibi_pic">
+                  <img src="assets/img/lechon.jpg" class="hibi_pic">
+                  <p>takutaku7</p>
+                  <p>セブのレッチョン最高すぎる説</p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+         <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10 today_hibi">
+              </h5>
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="assets/img/LRG_DSC05296.jpg" class="hibi_pic">
+                  <p>amibanana1</p>
+                  <p>俺のHTML最高っしょ</p>
+                </div>
+                <div class="col-md-4">
+                  <img src="assets/img/LRG_DSC05227.jpg" class="hibi_pic">
+                  <p>risachi55</p>
+                  <p>朝のパンって美味しいな〜</p>
+                </div>
+                <div class="col-md-4">
+                  <img src="assets/img/LRG_DSC05046.jpg" class="hibi_pic">
+                  <p>taichin6</p>
+                  <p>今日のナイトマーケット</p>
                 </div>
               </div>
             </div>
           </div>
+          <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10 topic_hibi">
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="assets/img/LRG_DSC04965.jpg" class="hibi_pic">
+                  <p>zakky9</p>
+                  <p>今日の夕食はローカルフード</p>
+                </div>
+                <div class="col-md-4">
+                  <img src="assets/img/IMG_4622.jpg" class="hibi_pic">
+                  <p>koni6</p>
+                  <p>シュリンプ美味すぎ</p>
+                </div>
+                <div class="col-md-4">
+                  <img src="assets/img/lechon.jpg" class="hibi_pic">
+                  <p>masumihole6</p>
+                  <p>セブのレッチョン最高すぎる説</p>
+                </div>
+               </div>
+            </div>
+          </div>
+
+           <div aria-label="Page navigation">
+          <ul class="pager">
+
+      
+            <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+          
+
+
+            
+            <li class="next disabled"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+          
+          </ul>
         </div>
       </div>
     </div>
+  </div>
+          
+
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
