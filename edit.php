9d76e75660fdd3a60a59bf5c0d9bff96964d582b
Takuto TADERA <ac15303@gm.ibaraki-ct.ac.jp>
2018-06-15 00:20:53 +0800

    correct the footer of theme.php and add pagenation func into edit.php

--- edit.php
+++ edit.php
@@ -10,7 +10,44 @@
     // サインインしているユーザーの情報を取得
     $signin_user = get_signin_user($dbh, $_SESSION['id']);
 
-    $sql = "SELECT * FROM `feeds` WHERE `user_id`= ? ORDER BY `created` DESC";
+      // ページネーション
+    $page = ''; //ページ番号が入る変数
+    $page_row_number = 12; //1ページあたりに表示するデータの数
+
+    if (isset($_GET['page'])){
+      $page = $_GET['page'];
+    }else{
+      // GET送信されてるページ数がない場合、1ページ目とみなす
+      $page = 1;
+    }
+
+    // max: カンマ区切りで羅列された数字の中から最大の数を返す
+    // 第一引数に入っている値を見て、第二引数と比較。もし第二引数の方が大きければ第二引数の値を返す
+    $page = max($page, 1);
+    $errors = array();
+
+    $count_sql = 'SELECT COUNT(*) AS `cnt` FROM `feeds` WHERE `user_id`=?';
+    $count_data = array($signin_user['id']);
+    $count_stmt = $dbh->prepare($count_sql);
+    $count_stmt->execute($count_data);
+    $feed_cnt = $count_stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($feed_cnt['cnt'] <= 0) {
+      $feed_cnt['cnt'] = 1;
+      $errors['feed'] = 'blank';
+    }
+
+    // ceil: 切り上げ
+    $max_page = ceil($feed_cnt['cnt'] / $page_row_number);
+
+
+    // 第一引数と第二引数を比較し、第二引数の方が小さければ、第二引数の値を返す
+    $page = min($page, $max_page);
+
+    // データを取得する開始番号を計算
+    $start = ($page -1)*$page_row_number;
+
+    $sql = "SELECT * FROM `feeds` WHERE `user_id`= ? ORDER BY `created` DESC LIMIT $start, $page_row_number";
     $data = array($signin_user['id']);
     $stmt = $dbh->prepare($sql);
     $stmt->execute($data);
@@ -68,14 +105,31 @@
           
 
         <div class="row">
-         <?php foreach($feeds as $feed){ ?>
-           <div class="col-md-4">
-              <img src="assets/img/<?php echo $feed['picture'] ?>" class="hibi_pic">
-              <a onclick="return confirm('削除してよろしいですか？')" href="delete.php?feed_id=<?php echo $feed["id"] ?>" class="btn btn-danger" style="margin: 10px 0px 10px 0px;">削除</a>
-           </div>
-         <?php } ?>
+          <?php if(isset($errors['feed'])){ ?>
+            <p>画像の投稿がありません</p>
+          <?php }else{ ?>
+            <?php foreach($feeds as $feed){ ?>
+             <div class="col-md-4">
+                <img src="assets/img/<?php echo $feed['picture'] ?>" class="hibi_pic">
+                <a onclick="return confirm('削除してよろしいですか？')" href="delete.php?feed_id=<?php echo $feed["id"] ?>" class="btn btn-danger " style="margin: 10px 0px 10px 0px;">削除</a>
+             </div>
+            <?php } ?>
+          <?php } ?>
         </div>
-
+          <div aria-label="Page navigation">
+           <ul class="pager">
+              <?php if ($page == 1){ ?>
+                 <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> 次へ</a></li>
+             <?php }else{ ?>
+                <li class="previous"><a href="edit.php?page=<?php echo $page - 1; ?>"><span aria-hidden="true">&larr;</span> 次へ</a></li>
+             <?php } ?>
+              <?php if ($page == $max_page){ ?>
+                <li class="next disabled"><a href="#">前へ <span aria-hidden="true">&rarr;</span></a></li>
+              <?php }else{ ?>
+                <li class="next"><a href="edit.php?page=<?php echo $page + 1; ?>">前へ <span aria-hidden="true">&rarr;</span></a></li>
+              <?php } ?>
+            </ul>
+          </div>
       </div>
      </div>
     <!-- footer -->
