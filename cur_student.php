e38593be8463afd480e5587faf9dddd6e571d416
Takuto TADERA <ac15303@gm.ibaraki-ct.ac.jp>
2018-06-19 11:08:00 +0800

    adjust positions of picture

--- cur_student.php
+++ cur_student.php
@@ -1,20 +1,20 @@
 <?php  
-    
-    require("dbconnect.php");
+
+require("dbconnect.php");
 
     // ページネーション処理
     $page = ''; //ページ番号が入る変数
-    $page_row_number = 16; //1ページあたりに表示するデータの数
+    $page_row_number = 12; //1ページあたりに表示するデータの数
 
     if (isset($_GET['page'])){
       $page = $_GET['page'];
     }else{
       //get送信されてるページ数がない場合、1ページめとみなす
-        $page = 1;
+      $page = 1;
     }
 
     if ($page < 0) {
-        $page = 1;
+      $page = 1;
     }
 
     // max:カンマ区切りで羅列された数字の中から最大の数を返す
@@ -47,85 +47,89 @@
     $cur_students = array();
 
     while (true) {
-        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($rec == false) {
-            break;
-        }
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($rec == false) {
+        break;
+      }
       $cur_students[] = $rec;
     }
 
-?>
+    ?>
 
-<!doctype html>
-<html lang="ja">
-  <head>
-  <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!doctype html>
+    <html lang="ja">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/hibi.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/alumnus.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/private.css"> 
-    <link rel="stylesheet" type="text/css" href="assets/css/page.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
-    <link href="assets/img/hibilogo.ico" rel="shortcut icon">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/hibi.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/alumnus.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/cur_students.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/private.css"> 
+      <link rel="stylesheet" type="text/css" href="assets/css/page.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
+      <link href="assets/img/hibilogo.ico" rel="shortcut icon">
 
-    <title>在校生の日々</title>
-  </head>
-  <body>
-    <div class="container-fluid">
+      <title>在校生の日々</title>
+    </head>
+    <body>
+  <div class="container-fluid">
+    <div class="row">
+     <?php include("navbar.php"); ?>
+     <div class="col-md-2"></div>
+
+     <div class="col-md-10 main-content">
       <div class="row">
-        <!-- サイドバーのコード読み込み -->
-        <?php include("navbar.php"); ?>
-        <div class="col-md-2"></div>
-
-        <div class="col-md-10 main-content">
-          <div class="row"></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
           <h1 class="h1 hibi_title" style="text-align: center; font-weight: bold;">在校生の日々をのぞいてみよう</h1>
-          <div class="col-md-12" style="display: block; margin: auto;">
-          <div style="width: 1000px; display: flex; flex-wrap: wrap;">
-            <?php foreach ($cur_students as $cur_student) { ?>
-              <?php if(!empty($cur_student['picture'])){ ?>
-                <div class="hibi_flame">
-                  <a href="my_page.php?user_id=<?php echo $cur_student["user_id"]; ?>"><img src="assets/img/<?php echo $cur_student["picture"]; ?>" class="hibi_pic"></a>
-                  <p class="hibi_username"><?php echo $cur_student["account_name"]; ?></p>
-                </div>
-              <?php }else{ ?>
-                <div class="hibi_flame">
-                  <a href="my_page.php?user_id=<?php echo $cur_student["id"]; ?>"><img src="assets/img/hibilog.png" class="hibi_pic"></a>
-                  <p class="hibi_username"><?php echo $cur_student["account_name"]; ?></p>
-                </div>
-              <?php } ?>
+          <div class="row mypage-content">
+            <?php foreach($cur_students as $cur_student){ ?>
+            <?php if(!empty($cur_student['picture'])){ ?>
+            <div class="col-md-4">
+              <div class="hibi_flame">
+                <a href="album.php?user_id=<?php echo $cur_student["user_id"]; ?>"><img src="assets/img/<?php echo $cur_student["picture"]; ?>" class="mypage-content-pic"></a>
+                <p class="hibi_username"><?php echo $cur_student["account_name"]; ?></p>
+              </div>
+            </div>
+            <?php }else{ ?>
+            <div class="col-md-4">
+              <div class="hibi_flame">
+                <a href="album.php?user_id=<?php echo $cur_student["user_id"]; ?>"><img src="assets/img/hibilog.png" class="mypage-content-pic"></a>
+                <p class="hibi_username"><?php echo $cur_student["account_name"]; ?></p>
+              </div>
+            </div>
             <?php } ?>
-
-          </div>
+            <?php } ?>
           </div>
           <div aria-label="Page navigation">
-            <ul class="pager">
-              <?php if ($page == 1){ ?>
-              <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span>前へ</a></li>
-              <?php }else{ ?>
-              <li class="previous"><a href="cur_student.php?page=<?php echo $page-1; ?>"><span aria-hidden="true">&larr;</span>前へ</a></li>
-              <?php } ?>
-
-              <?php if ($page == $all_page_number){ ?>
-                <li class="next disabled"><a href="#">次へ<span aria-hidden="true">&rarr;</span></a></li>
-              <?php }else{ ?>
-                <li class="next"><a href="cur_student.php?page=<?php echo $page+1; ?>">次へ<span aria-hidden="true">&rarr;</span></a></li>
-              <?php } ?>
-            </ul>
-          </div>
+           <ul class="pager">
+            <?php if ($page == 1){ ?>
+            <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> 次へ</a></li>
+            <?php }else{ ?>
+            <li class="previous"><a href="cur_student.php?page=<?php echo $page - 1; ?>"><span aria-hidden="true">&larr;</span> 次へ</a></li>
+            <?php } ?>
+            <?php if ($page == $all_page_number){ ?>
+            <li class="next disabled"><a href="#">前へ <span aria-hidden="true">&rarr;</span></a></li>
+            <?php }else{ ?>
+            <li class="next"><a href="cur_student.php?page=<?php echo $page + 1; ?>">前へ <span aria-hidden="true">&rarr;</span></a></li>
+            <?php } ?>
+          </ul>
         </div>
       </div>
     </div>
-    <!-- footer -->
-    <?php include("footer.php"); ?>
+  </div>
+</div>
+</div>
+      <!-- footer -->
+      <?php include("footer.php"); ?>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  </body>
-</html>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    </body>
+    </html>
